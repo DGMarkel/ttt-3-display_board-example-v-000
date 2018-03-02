3ee3f3acd2d6ac45e186d104c869380aa40178fc
David Markel <davidgmarkel@gmail.com>
2018-03-02 19:52:05 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,10 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  pre = "```"
   vertical = "   |   |   "
   horizontal = "-----------"
-  puts pre
   puts vertical
   puts horizontal
   puts vertical
   puts horizontal
   puts vertical
-  puts pre
 end
